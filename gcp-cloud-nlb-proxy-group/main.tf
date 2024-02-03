85abc8a2016591e20d754dd5a276d582e26cf537
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-03 14:29:02 +0200

    [terraform apply] patch: fix health check for lb and add named port

--- gcp-cloud-nlb-proxy-group/main.tf
+++ gcp-cloud-nlb-proxy-group/main.tf
@@ -11,8 +11,10 @@ resource "google_compute_backend_service" "backendservice" {
 
 resource "google_compute_health_check" "tcp_health_check" {
   name               = "${var.name}-tcp-health-check"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 10 # 50 seconds
 
   tcp_health_check {
     port = "1883"
@@ -39,11 +41,17 @@ resource "google_compute_global_forwarding_rule" "forwarding_rule" {
 
 resource "google_compute_instance_group_manager" "default" {
   name                = "${var.name}-instance-group"
+
   base_instance_name  = var.name
   zone                = var.zone
 
   version {
     instance_template   = var.instance_template
+  }
+
+  named_port {
+    name = "MQTT"
+    port = 1883
   }
 
   target_size = var.replicas
