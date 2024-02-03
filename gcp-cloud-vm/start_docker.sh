08d1d143c5cc85da54e9e817f2bbdbefc7717484
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-03 12:11:33 +0200

    [terraform apply]

--- gcp-cloud-vm/start_docker.sh
+++ gcp-cloud-vm/start_docker.sh
@@ -23,9 +23,12 @@ sudo git clone https://github.com/OpenSrcerer/paddy-infrastructure.git
 cd ./paddy-infrastructure/docker-paddy
 
 # Get environment variables
-sudo BACKEND_MQTT_HOST=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host)
-sudo BACKEND_MQTT_PORT=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host)
-sudo BACKEND_MQTT_SUBSCRIPTIONS=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_subscriptions)
+BACKEND_MQTT_HOST=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host)
+BACKEND_MQTT_PORT=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host)
+BACKEND_MQTT_SUBSCRIPTIONS=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_subscriptions)
+
+# Print Env variables
+echo $BACKEND_MQTT_HOST
 
 # Loop till docker starts up lmao
 SUCCESS=0
