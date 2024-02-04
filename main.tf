0229de0c919e86d45126a9cbbfe7b20b14de0d61
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-04 13:57:20 +0200

    [apply] patch: remove ssl cert for now, only expose mqtts

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 module "paddy_nw" {
   source                  = "./gcp-cloud-network"
   name                    = "paddy-network"
-  allowed_ports_tcp_anyip = ["22", "1883", "8883"]
+  allowed_ports_tcp_anyip = ["22", "8883"]
 }
 
 module "paddy_backend_instance_template" {
@@ -38,10 +38,9 @@ module "paddy_nlb_proxy_group" {
   instance_template = module.paddy_backend_instance_template.self_link
 
   target_ports = {
-    "mqtt"  = 1883
     "mqtts" = 8883
   }
-  health_check_port      = 1883
+  health_check_port      = 8883
   ssl_certificate_domain = "mqtt.danielstefani.online"
 
   replicas = 1
