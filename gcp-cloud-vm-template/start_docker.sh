5ec2ef652c92fdaa0369c59b00eb081068417e0d
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-06 21:33:16 +0200

    [apply] patch: update auth key and run command

--- gcp-cloud-vm-template/start_docker.sh
+++ gcp-cloud-vm-template/start_docker.sh
@@ -1,33 +1,15 @@
 #!  /bin/bash
 
-# Step 1: Install Compose
-alias docker-compose='docker run --rm docker compose'
-
-# Step 2: Run actual apps
-
 cd /home/chronos
 
 sudo git clone https://github.com/OpenSrcerer/paddy-infrastructure.git
 
 cd ./paddy-infrastructure/docker
 
-# Get environment variables
-export BACKEND_MQTT_HOST=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host)
-export BACKEND_MQTT_PORT=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_port)
-export BACKEND_MQTT_SUBSCRIPTIONS=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_subscriptions)
-export BACKEND_MQTT_AUTHENTICATION_KEY=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_authentication_key)
-
-# Loop till docker starts up lmao
-SUCCESS=0
-ATTEMPTS=0
-
-until [ $SUCCESS -eq 1 ] || [ $ATTEMPTS -eq 10 ]; do
-  sudo -E docker-compose up -d && SUCCESS=1 || ATTEMPTS=$((ATTEMPTS + 1))
-  sleep 10
-done
-
-if [ $SUCCESS -eq 0 ]; then
-  cd ../../
-  echo "Docker didn't boot up in time, exiting..." >> error.txt
-  exit 1
-fi
+# Pull compose and run with environment variables
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/$PWD" -w="/$PWD" \
+  -e BACKEND_MQTT_HOST=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_host) \
+  -e BACKEND_MQTT_PORT=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_port) \
+  -e BACKEND_MQTT_SUBSCRIPTIONS=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_subscriptions) \
+  -e BACKEND_MQTT_AUTHENTICATION_KEY=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/backend_mqtt_authentication_key | tr -d '\n') \
+  docker/compose:latest up -d
