317ae1d4bf35bacca8f2c0b0435c60da5a406a14
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-07 00:18:54 +0200

    [apply] patch: fix domain name missing dot

--- gcp-cloud-internal-dns/main.tf
+++ gcp-cloud-internal-dns/main.tf
@@ -1,6 +1,6 @@
 resource "google_dns_managed_zone" "private" {
     name        = "${var.name}-dns-zone"
-    dns_name    = var.domain_name
+    dns_name    = "${var.domain_name}."
     visibility  = "private"
 
     private_visibility_config {
@@ -13,7 +13,7 @@ resource "google_dns_managed_zone" "private" {
 resource "google_dns_record_set" "private" {
     for_each = var.dns_records
 
-    name         = "${each.value}.${var.domain_name}"
+    name         = "${each.value}.${var.domain_name}."
     type         = "A"
     ttl          = 300
     managed_zone = google_dns_managed_zone.private.name
