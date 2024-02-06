d1b6cdb372eac93d8692e107d2b30e1ff75e72e8
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-06 20:53:28 +0200

    [apply]

--- gcp-cloud-vm-template/start_docker.sh
+++ gcp-cloud-vm-template/start_docker.sh
@@ -5,9 +5,9 @@ sudo curl -sSL \
   https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 \
   -o /var/lib/google/docker-compose
 sudo chmod o+x /var/lib/google/docker-compose
-mkdir -p ~/.docker/cli-plugins
+mkdir -p /home/chronos/.docker/cli-plugins
 ln -sf /var/lib/google/docker-compose \
-  ~/.docker/cli-plugins/docker-compose
+  /home/chronos/.docker/cli-plugins/docker-compose
 
 # Step 2: Run actual apps
 
