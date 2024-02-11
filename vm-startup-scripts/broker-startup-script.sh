7784237f2d1f664f0a80a61c21818b20f1bff021
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-11 16:57:35 +0200

    [apply] patch: add internal ip to broker nodes

--- vm-startup-scripts/broker-startup-script.sh
+++ vm-startup-scripts/broker-startup-script.sh
@@ -8,4 +8,5 @@ cd ./paddy-infrastructure/docker
 
 # Pull compose and run with environment variables
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/$PWD" -w="/$PWD" \
+  -e BROKER_INTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/internal_ip) \
   docker/compose:latest -f broker-docker-compose.yml up -d
