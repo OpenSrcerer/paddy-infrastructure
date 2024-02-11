926a78eaa831b97d33b929e713bdee6b0f7ab161
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-11 17:37:53 +0200

    [apply] patch: move internal ip

--- vm-startup-scripts/etcd-startup-script.sh
+++ vm-startup-scripts/etcd-startup-script.sh
@@ -8,5 +8,5 @@ cd ./paddy-infrastructure/docker
 
 # Pull compose and run with environment variables
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/$PWD" -w="/$PWD" \
-  -e ETCD_HOST_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/internal_ip) \
+  -e ETCD_HOST_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip) \
   docker/compose:latest -f etcd-docker-compose.yml up -d
