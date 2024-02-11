9233b2a61314c2ef84776f9da1ec320ff5a4fd6d
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-11 17:02:21 +0200

    [apply] patch: fix emqx node name variable

--- vm-startup-scripts/broker-startup-script.sh
+++ vm-startup-scripts/broker-startup-script.sh
@@ -7,6 +7,6 @@ sudo git clone https://github.com/OpenSrcerer/paddy-infrastructure.git
 cd ./paddy-infrastructure/docker
 
 # Pull compose and run with environment variables
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/$PWD" -w="/$PWD" \
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:/$PWD" -w="/$PWD" \ 
   -e BROKER_INTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/internal_ip) \
   docker/compose:latest -f broker-docker-compose.yml up -d
