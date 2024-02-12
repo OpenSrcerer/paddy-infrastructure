d505762f495a439bfdfc8a496fdc083ceeffa977
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-12 18:09:40 +0200

    [apply] feat: add missing image on boot disk config

--- gcp-cloud-vm/main.tf
+++ gcp-cloud-vm/main.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       size  = var.disk_size
-      image = "cos-cloud/cos-stable"
+      image = var.image
     }
   }
 
